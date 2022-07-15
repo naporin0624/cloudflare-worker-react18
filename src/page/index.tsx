57037a2b851c86f51b6db5388476275d6f14975d
naporin0624 <naporin0624@gmail.com>
2022-07-16 05:15:32 +0900

    :+1: hydrate できる

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,5 +1,5 @@
 import { useReadData, fromAsyncFunction } from "@naporin0624/react-flowder";
-import React, { Suspense, memo } from "react";
+import React, { Suspense, memo, useState, useCallback } from "react";
 
 import type { FC } from "react";
 
@@ -14,9 +14,10 @@ const Page: FC = () => {
   return (
     <section>
       <h1>Hello World</h1>
+      <Counter />
       {arr.map((ms) => (
         <Suspense fallback={<p>waiting {ms} ms</p>} key={ms}>
-          <Component ms={ms} />
+          <SuspendComponent ms={ms} />
         </Suspense>
       ))}
     </section>
@@ -25,8 +26,28 @@ const Page: FC = () => {
 
 export default memo(Page);
 
-const Component: FC<{ ms?: number }> = ({ ms }) => {
+const SuspendComponent: FC<{ ms?: number }> = ({ ms }) => {
   const d = useReadData(delay(ms));
 
   return <p>delay: {d}</p>;
+};
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const increment = useCallback(() => {
+    setCount((c) => c + 1);
+  }, []);
+  const decrement = useCallback(() => {
+    setCount((c) => c - 1);
+  }, []);
+
+  return (
+    <div>
+      <p>count: {count}</p>
+      <div>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
+    </div>
+  );
 };
--- src/server.tsx
+++ src/server.tsx
@@ -1,12 +1,13 @@
 import { Hono } from "hono";
-import { logger } from "hono/logger";
+// import { logger } from "hono/logger";
+import { serveStatic } from "hono/serve-static";
 import React from "react";
 import { renderToReadableStream } from "react-dom/server";
 
 import App from "./App";
 
 const app = new Hono();
-app.use("*", logger());
+app.use("/assets/*", serveStatic({ root: "./" }));
 
 app.get("/", async () => {
   const stream = await renderToReadableStream(
@@ -14,9 +15,13 @@ app.get("/", async () => {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* dist の中を見て手で変更してる */}
+        <script type="module" src="/assets/client.d0bd8681.js"></script>
       </head>
       <body>
-        <App />
+        <div id="app">
+          <App />
+        </div>
       </body>
     </html>
   );
